959145c202ef41a96488b90f10a66a1429cd45d3
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-10-20 15:17:39 -0300

    inserindo template para cadastro de novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -11,14 +11,19 @@ class Jogo:
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def listar():
     jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
     jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
     jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 
     lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
 app.run()
