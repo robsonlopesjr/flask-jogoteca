a6f97385da031083c0244abf85c8a15ac9da0952
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-10-20 16:35:08 -0300

    inserindo template de login

--- jogoteca.py
+++ jogoteca.py
@@ -41,4 +41,17 @@ def criar():
     return redirect('/')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if '123' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+
 app.run(debug=True)
